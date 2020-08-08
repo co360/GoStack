61d521f61b774579c5443975658e1e7691af5b18
Leonardo Barbosa <leonardo.pb@icloud.com>
2020-08-08 20:38:50 -0300

    feat: code patterns config

--- node-1st/src/server.ts
+++ node-1st/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack'})
-})
+app.get('/', (req, res) => res.json({ message: 'Hello GoStack' }));
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!')
+  console.log('Server started on port 3333!');
 });
