134c0fbb5c2d2b32bbdcd5afadbfda9791f46966
Leonardo Barbosa <leonardo.pb@icloud.com>
2020-08-09 16:57:07 -0300

    feat: add Appointment entity and repository

--- node-1st/src/server.ts
+++ node-1st/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello GoStack' }));
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
